99fdb8c5f7ae1322ac89993fd78d4dea7917c7a8
GokulBVMoback <gokulbv@moback.com>
2022-11-04 01:22:13 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Demo_Core_Api.Models;
+using System.Data.SqlClient;
+using System.Net.WebSockets;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Demo_Core_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        public IConfiguration _configuration { get; }
+        SqlConnection con;
+        public EmployeeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+             con= new SqlConnection(_configuration.GetConnectionString("DefaultParkingConnection"));
+
+        }
+
+        [HttpGet]
+        public JsonResult GetAllEmployee()
+        {
+            List<Employee> employees = new List<Employee>();
+            try
+            {
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "SELECT * FROM tbl_Emp";
+                con.Open();
+                SqlDataReader sdr = cmd.ExecuteReader();
+                while (sdr.Read())
+                {
+                    employees.Add(new Employee
+                    {
+                        Id = Convert.ToInt32(sdr["Id"]),
+                        Name = Convert.ToString(sdr["Name"]),
+                        Age = Convert.ToInt32(sdr["Age"])
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+            return new JsonResult(employees);
+
+        }
+        //[HttpGet]
+        //public JsonResult GetEmployee(int id)
+        //{
+        //    List<Employee> employees = new List<Employee>();
+        //    try
+        //    {
+        //        SqlCommand cmd = con.CreateCommand();
+        //        cmd.CommandType = CommandType.Text;
+        //        cmd.CommandText = "SELECT * FROM tbl_Emp Where(ID="+id+");";
+        //        con.Open();
+        //        SqlDataReader sdr = cmd.ExecuteReader();
+        //        while (sdr.Read())
+        //        {
+        //            employees.Add(new Employee
+        //            {
+        //                Id = Convert.ToInt32(sdr["Id"]),
+        //                Name = Convert.ToString(sdr["Name"]),
+        //                Age = Convert.ToInt32(sdr["Age"])
+        //            });
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return new JsonResult(ex.Message);
+        //    }
+        //    finally
+        //    {
+        //        con.Close();
+        //    }
+        //    return new JsonResult(employees);
+
+        //}
+        [HttpPost]
+        public Boolean AddEmployee(Employee employee)
+        {
+            try
+            {
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "Insert into tbl_Emp(Name, Age) values('" + employee.Name + "'," + employee.Age + ")";
+                con.Open();
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                    return false;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        [HttpPut]
+        public Boolean UpdateEmployee(int id,Employee employee)
+        {
+            try
+            {
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "Update tbl_Emp Set Name='"+ employee.Name + "', Age=" + employee.Age + "Where(ID="+id + ");";
+                con.Open();
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        [HttpDelete]
+        public Boolean DeleteEmployee(int id)
+        {
+            try
+            {
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "Delete from tbl_Emp Where(ID="+id+");";
+                con.Open();
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo_Core_Api.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; } 
+        [Required]
+        public string Name { get; set; }
+        [Required]
+
+        public int Age { get; set; }
+    }
+}
